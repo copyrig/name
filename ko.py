3e592a491040994337b63374138d10ac8cee5b68
Dong-jun Lim <ciqete@gmail.com>
2017-01-06 11:06:47 +0900

    Split file

---
+++ ko.py
@@ -0,0 +1,47 @@
+import lang
+
+class ComposerElement_ko(ComposerElementBase):
+    """Korean name composer - inspired by 이강성, 『파이썬 3 바이블』"""
+    characters = ( \
+    ('ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', \
+    'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'), \
+    ('ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', \
+    'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'), \
+    ('', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', \
+    'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ') \
+    )
+    initial = ListBase()
+    medial = ListBase()
+    final = ListBase()
+    def __init__(self, initial=None, medial=None, final=None):
+        pass # See the metaclass
+    def compose(self):
+        """Compose the Korean name"""
+        characters = self.characters
+        list_original = [self.initial, self.medial, self.final]
+        list_process = list(list_original)
+        ingredient = [None, None, None]
+
+        # Check type and switch
+        for (idx, elem) in enumerate(list_process):
+            if isinstance(elem, ListBase):
+                list_process[idx] = (list_process[idx].element)[0]
+            elif elem is None:
+                list_process[idx] = list()
+            else:
+                raise NamingLibException("Check composer's input type")
+
+        # Change str to index
+        for (elem, compare) in zip(list_process, (characters[0], characters[1], characters[2])):
+            self.digitize(elem, compare)
+
+        # Check whether index is out of range
+        for elem in list_process:
+            for idx in range(len(elem)):
+                pass
+
+        result_int = 0xac00 + ((ingredient[0] * 21) + ingredient[1]) * 28 + ingredient[2]
+        result_char = chr(result_int)
+
+        self.result = result_char
+        return result_char
--- lang.py
+++ lang.py
@@ -1,7 +1,6 @@
 """Language set"""
 
 import collections
-import random
 
 class NamingLibException(Exception):
     """Basic exception class for naming"""
@@ -95,51 +94,3 @@ class ComposerElementBase(metaclass=AutoMemberSetType):
         for (idx, elem) in enumerate(list_control):
             if list_control[idx] in range(len(list_compare)):
                 list_control[idx] = list_compare.index(list_control[idx])
-
-class ComposerElement_ko(ComposerElementBase):
-    """Korean name composer - inspired by 이강성, 『파이썬 3 바이블』"""
-    characters = ( \
-    ('ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', \
-    'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'), \
-    ('ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', \
-    'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'), \
-    ('', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', \
-    'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ') \
-    )
-    initial = ListBase()
-    medial = ListBase()
-    final = ListBase()
-    def __init__(self, initial=None, medial=None, final=None):
-        pass # See the metaclass
-    def compose(self):
-        """Compose the Korean name"""
-        characters = self.characters
-        list_original = [self.initial, self.medial, self.final]
-        list_process = list(list_original)
-        ingredient = [None, None, None]
-
-        # Check type and switch
-        for (idx, elem) in enumerate(list_process):
-            if isinstance(elem, ListBase):
-                list_process[idx] = (list_process[idx].element)[0]
-            elif elem is None:
-                list_process[idx] = list()
-            else:
-                raise NamingLibException("Check composer's input type")
-
-        # Change str to index
-        for (elem, compare) in zip(list_process, (characters[0], characters[1], characters[2])):
-            self.digitize(elem, compare)
-
-        # Check whether index is out of range
-        for elem in list_process:
-            pass
-
-        result_int = 0xac00 + ((ingredient[0] * 21) + ingredient[1]) * 28 + ingredient[2]
-        result_char = chr(result_int)
-
-        self.result = result_char
-        return result_char
-
-x = ComposerElement_ko(IncludeList('ㄱ'), IncludeList('ㅏ'))
-print(x.compose())
